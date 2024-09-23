00773ea68623d2d844d6b4c15645e32ad7898d8a
Negar Javadzadeh <negar.jvd.79@gmail.com>
2024-09-23 06:50:47 +0330

    optimizer

--- resources/views/design/create/step4.blade.php
+++ resources/views/design/create/step4.blade.php
@@ -5,6 +5,25 @@
         </h2>
     </x-slot>
 
+    <script type="module">
+        $(document).ready(function() {
+            $('#optimizer').on('click', function (){
+                $('#optimizer_result').removeClass('hidden')
+
+                $('#beam_w').val(85);
+                $('#column_w').val(85);
+                $('#top_plate_w').val(85);
+                $('#long_sill_w').val(85);
+
+                $('#beam_h').val(127);
+                $('#column_h').val(127);
+                $('#top_plate_h').val(127);
+                $('#long_sill_h').val(127);
+
+            })
+        })
+    </script>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -80,9 +99,14 @@
                             </a>
 
                             <x-primary-button>{{ __('Save and next') }}</x-primary-button>
+
+                            <x-secondary-button id="optimizer">{{__('Optimizer')}}</x-secondary-button>
                         </div>
                     </form>
 
+                    <div id="optimizer_result" class="hidden mt-4">
+                        Weight: 13.17 kg, width: 85 mm, height: 127 mm, Beam Length: 2.00 m, Bending Utilisation(beam): 35.78%, Bending Status (beam): Acceptable, Shear Utilisation(beam) : 22.97%, Shear Status (beam): Acceptable, SLS Utilisation(beam): 99.61%, SLS Status(beam): Acceptable, b.clm: 85 mm, w.clm: 127.00 mm, w.tb: 127 mm, t.t: 190.50 mm, Compression Utilisation(column): 3.79%, Compression status(column): Acceptable, Buckling Utilisation in plane(column): 2.48%, Buckling status in plane(column): Acceptable,Buckling Utilisation out of plane (column): 4.72%, Buckling status out of plane(column): Acceptable, Final Utilisation: 99.61%, Final utilisation status: Acceptable
+                    </div>
                 </div>
             </div>
         </div>
