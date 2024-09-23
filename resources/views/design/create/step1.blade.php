9be4782af019d150b918af613ffb68fc1f87e4f8
Negar Javadzadeh <negar.jvd.79@gmail.com>
2024-09-23 07:11:51 +0330

    wood details show in step 1

--- resources/views/design/create/step1.blade.php
+++ resources/views/design/create/step1.blade.php
@@ -5,6 +5,17 @@
         </h2>
     </x-slot>
 
+    <script type="module">
+        $(document).ready(function() {
+            $('#woods').on('click', function (){
+                const id = $('#woods').val()
+                $('.details').addClass('hidden')
+                const p_id = 'details_'+id
+                $('#'+p_id).removeClass('hidden')
+            })
+        })
+    </script>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -18,7 +29,7 @@
                         @csrf
 
                         <div class="flex flex-row">
-                            <div class="basis-1/2 mr-2">
+                            <div class="basis-1/3 mr-2">
                                 <x-input-label for="woods" :value="__('Select woods')" />
                                 <x-select id="woods" name="woods" class="mt-1 block w-full" required autofocus autocomplete="woods">
                                     @foreach(App\Models\Wood::all() as $wood)
@@ -28,12 +39,26 @@
                                 <x-input-error class="mt-2" :messages="$errors->get('woods')" />
                             </div>
 
-                            <div class="basis-1/2 mr-2">
+                            <div class="basis-1/3 mr-2">
                                 <div id="create_wood" class="flex flex-row justify-start text-center mt-6">
                                     <a class="border border-slate-600 p-1 rounded-lg hover:bg-gray-100" href="{{route('wood-management.index')}}" target=”_blank”>
                                         Create new
                                     </a>
                                 </div>
+                            </div>
+
+                            <div class="basis-1/3 mr-2">
+                                @foreach(App\Models\Wood::all() as $wood)
+                                    <p id="details_{{$wood->id}}" class="hidden details">
+                                        bending_strength : {{$wood->bending_strength}}
+                                        tension_parallel : {{$wood->tension_parallel}}
+                                        tension_perpendicular : {{$wood->tension_perpendicular}}
+                                        compression_parallel : {{$wood->compression_parallel}}
+                                        compression_perpendicular : {{$wood->compression_perpendicular}}
+                                        shear_strength : {{$wood->shear_strength}}
+                                        e_modulus : {{$wood->e_modulus}}
+                                    </p>
+                                @endforeach
                             </div>
                         </div>
 
