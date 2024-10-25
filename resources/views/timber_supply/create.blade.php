d079ef8f0275b0df4ac3d273763ca1e6bf90a36f
Negar Javadzadeh <negar.jvd.79@gmail.com>
2024-10-26 01:59:20 +0330

    showing material details for timber provider

--- resources/views/timber_supply/create.blade.php
+++ resources/views/timber_supply/create.blade.php
@@ -15,6 +15,16 @@
                     $('#radius').val(0).change()
                 }
             })
+
+            $('#woods').on('click', function (){
+                const ids = $('#woods').val()
+                $('.details').addClass('hidden')
+
+                for(var i=0; i<ids.length; i++){
+                    const p_id = 'details_'+ids[i]
+                    $('#'+p_id).removeClass('hidden')
+                }
+            })
         })
     </script>
 
@@ -66,7 +76,7 @@
                         </div>
 
                         <div class="flex flex-row">
-                            <div class="basis-1/2 mr-2">
+                            <div class="basis-1/3 mr-2">
                                 <x-input-label for="woods" :value="__('Select woods')" />
                                 <x-select multiple id="woods" name="woods[]" class="mt-1 block w-full" required autofocus autocomplete="woods">
                                     @foreach($woods as $wood)
@@ -74,6 +84,25 @@
                                     @endforeach
                                 </x-select>
                                 <x-input-error class="mt-2" :messages="$errors->get('woods')" />
+                            </div>
+                            <div class="basis-2/3 ml-2 h-[350px] overflow-y-auto">
+                                @foreach($woods as $wood)
+                                    <p id="details_{{$wood->id}}" class="hidden details">
+                                        <b>name : {{$wood->name}}</b><br>
+                                        bending strength : {{$wood->bending_strength}} <br>
+                                        tension parallel : {{$wood->tension_parallel}} | tension perpendicular : {{$wood->tension_perpendicular}} <br>
+                                        compression parallel : {{$wood->compression_parallel}} | compression perpendicular : {{$wood->compression_perpendicular}} <br>
+                                        shear strength : {{$wood->shear_strength}} <br>
+                                        e modulus : {{$wood->e_modulus}} | e modulus 5% : {{$wood->e_modulus_5}} <br>
+                                        partial factor : {{$wood->partial_factor}} | density : {{$wood->density}}  <br>
+                                        modification factor permanent, medium and instantaneous term : {{$wood->modification_factor_permanent_term}} | {{$wood->modification_factor_medium_term}} | {{$wood->modification_factor_instantaneous_term}}<br>
+                                        creep factor : {{$wood->creep_factor}} | creep factor solid timber : {{$wood->creep_factor_solid_timber}} <br>
+                                        end distance : {{$wood->dtl_e}} | edge distance : {{$wood->dtl_v}} <br>
+                                        vertical edge distance : {{$wood->dtl_g}} | peg spacing distance : {{$wood->dtl_s}}
+                                        <br>
+                                        <br>
+                                    </p>
+                                @endforeach
                             </div>
                         </div>
 
