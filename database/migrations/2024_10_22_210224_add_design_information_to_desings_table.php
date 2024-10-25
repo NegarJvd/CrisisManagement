e722418337879c130e8e024562644d4766ae2333
Negar Javadzadeh <negar.jvd.79@gmail.com>
2024-10-25 23:38:20 +0330

    delete g-code column from designs table

--- database/migrations/2024_10_22_210224_add_design_information_to_desings_table.php
+++ database/migrations/2024_10_22_210224_add_design_information_to_desings_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('designs', function (Blueprint $table) {
             $table->mediumText('information')->nullable();
-            $table->mediumText('g_code')->nullable();
         });
     }
 
@@ -24,7 +23,6 @@ return new class extends Migration
     {
         Schema::table('designs', function (Blueprint $table) {
             $table->dropColumn('information');
-            $table->dropColumn('g_code');
         });
     }
 };
